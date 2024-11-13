b656cbf001ae43e40845cc7ec10a71ace61d8256
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-11-13 12:56:48 +0500

    integrate company api

---
+++ src/pages/api/updateCompany.ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    const { id, name, type, address, status, email, contact, companyLogo } =
+      req.body;
+
+    try {
+      const accessToken = req.cookies.accessToken;
+
+      if (!accessToken) {
+        throw new Error("Token not found. Please log in.");
+      }
+
+      // Send credentials to external API
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/company/update/`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify({
+            id,
+            name,
+            type,
+            address,
+            status,
+            email,
+            contact,
+            companyLogo,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const apiResponse = await response.json();
+        return res.status(200).json({
+          data: apiResponse.data,
+          message: "Successfully updated company",
+        });
+      } else {
+        const errorData = await response.json();
+        return res.status(response.status).json({
+          message: errorData.message || "Failed to update company",
+        });
+      }
+    } catch (error) {
+      console.error("Error authenticating:", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
+    res.setHeader("Allow", ["PUT"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
